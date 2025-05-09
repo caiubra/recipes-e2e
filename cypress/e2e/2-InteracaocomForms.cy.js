1d171e66e9475af7363cf929f599421fada463e8
Caio B. Costa <Caio.Costa@BEMATIZESP.local>
2025-05-08 21:02:38 -0300

    Atualizando atividade

---
+++ cypress/e2e/2-InteracaocomForms.cy.js
@@ -0,0 +1,15 @@
+describe('Deve visitar página de test e pesquisar por uma receita existente', () => {
+    beforeEach(() => {
+        cy.visit('https://meulivrodereceitas.vercel.app/')
+    })
+    
+    it('Deve visitar a página', () => {
+        cy.url().should('include','/home')
+    })
+    
+    it('Deve pesquisar uma receita existente, clicar na mesma e checar detalhes', () => {
+        cy.get('input[placeholder="Procurar receitas ou ingredientes..."]').type('macarons')
+        cy.contains('a','Macarons').click()
+        cy.contains('h2','Modo de preparo').should('be.visible')
+    })
+})
--- cypress/e2e/2-Navegacao.cy.js
+++ cypress/e2e/2-Navegacao.cy.js
@@ -7,17 +7,17 @@ describe('Teste de Navegação', () => {
 
 // Clicar no link Minhas receitas e verifica o redirecionmaneto para a home 
   it('Deve redirecionar para o login ao clicar em Minhas receitas', () => {
-    cy.contains('Minhas receitas').click()
+    cy.contains('a','Minhas receitas').click()
     cy.url().should('include','/login')
 })
 // Clicar no link Meus favoritos e verifica e o redirecionado para home
   it('Deve recirecionar para o login ao clicar em Meus favoritos', () => {
-    cy.contains('Meus favoritos').click()
+    cy.contains('a','Meus favoritos').click()
     cy.url().should('include','/login')
 })
 // Clicar no link Minha conta e verifica e o redirecionado para home
 it('Deve recirecionar para o login ao clicar em Minha Conta', () => {
-    cy.contains('Minha conta').click()
+    cy.contains('a','Minha conta').click()
     cy.url().should('include','/login')
 })
 
