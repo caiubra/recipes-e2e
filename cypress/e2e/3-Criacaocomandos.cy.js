6e2cfb19987e88da234e26c823a0766c87f008be
Caio B. Costa <Caio.Costa@BEMATIZESP.local>
2025-05-15 12:01:20 -0300

    Atualizando atividade

--- cypress/e2e/3-Criacaocomandos.cy.js
+++ cypress/e2e/3-Criacaocomandos.cy.js
@@ -1,10 +1,10 @@
 describe('Criação de Comandos personalizados', () => {
     it('Deve navegar até a home e pesquisar por uma receita', () => {
       cy.navegarParaHome()
-      cy.pesquisarReceita('macarons')
+      cy.pesquisarReceita('Macarons')
   
       cy.url().should('include', '/receita')
-      cy.contains('macarons').should('exist')
+      cy.get('h1').should('contain','Macarons')
     })
   })
   
