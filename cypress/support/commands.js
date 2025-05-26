3bc2e35ecb7ead6220559844e6d314fcb8a1fc2c
Caio B. Costa <Caio.Costa@BEMATIZESP.local>
2025-05-26 10:42:16 -0300

    ajustando identacao

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,12 @@
 // Comando para visitar home e validar a URL
-Cypress.Commands.add('navegarParaHome', () => {
-    cy.visit('https://meulivrodereceitas.vercel.app/')
-    cy.url().should('include', '/home')
-  })
-  
+Cypress.Commands.add("navegarParaHome", () => {
+  cy.visit("https://meulivrodereceitas.vercel.app/");
+  cy.url().should("include", "/home");
+});
+
 // Comando para pesquisar uma receita e clicar no primeiro item relacionado
 
-  Cypress.Commands.add('pesquisarReceita', (termo) => {
-    cy.get('input[placeholder*="Procurar receitas"]').type(termo)
-    cy.get('a')
-      .contains(termo)
-      .should('be.visible')
-      .click()
-  })
+Cypress.Commands.add("pesquisarReceita", (termo) => {
+  cy.get('input[placeholder*="Procurar receitas"]').type(termo);
+  cy.get("a").contains(termo).should("be.visible").click();
+});
